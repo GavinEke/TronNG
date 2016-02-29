a10b02ce04edd9f2946ca288c1e976ad2da8888f
Gavin Eke <GavinEke@users.noreply.github.com>
2016-02-29 16:07:00 +1100

    v2.0.3
    - Simplified Prerequisite Check
    - Added Verbose Output

--- TronNG.ps1
+++ TronNG.ps1
@@ -5,42 +5,44 @@ Function Test-Administrator {
     $AdminUserTest = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $AdminUserTest).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
 }
-$TestAdmin = Test-Administrator 
 
-If ($TestAdmin -eq $True) { #Admin Check
-    If ([Environment]::OSVersion.Version.Major -eq 10) { #Win10 Check
-        # Step 0 - Create Checkpoint
-        Checkpoint-Computer -Description "TronNG"
+If ((Test-Administrator -eq $True) -and ([Environment]::OSVersion.Version.Major -eq 10)) { #Prerequisite Check
+    # Step 0 - Create Checkpoint
+    Write-Verbose "Step 0 - Create Checkpoint"
+    Checkpoint-Computer -Description "TronNG"
 
-        # Step 1 - Update
-        wuauclt.exe /detectnow /updatenow
+    # Step 1 - Update
+    Write-Verbose "Step 1 - Update"
+    wuauclt.exe /detectnow /updatenow
 
-        # Step 2 - Virus Scan
-        Update-MpSignature
-        Start-MpScan
+    # Step 2 - Virus Scan
+    Write-Verbose "Step 2 - Virus Scan"
+    Update-MpSignature
+    Start-MpScan
 
-        # Step 3 - Clean-up
-        Remove-Item $env:temp\* -Recurse
-        Clear-RecycleBin
+    # Step 3 - Clean-up
+    Write-Verbose "Step 3 - Clean-up"
+    Remove-Item $env:temp\* -Recurse
+    Clear-RecycleBin
 
-        # Step 4 - Repair
-        Repair-Volume -DriveLetter C –Scan
+    # Step 4 - Repair
+    Write-Verbose "Step 4 - Repair"
+    Repair-Volume -DriveLetter C –Scan
 
-        # Step 5 - Defrag
-        $IsSSD = Get-Disk | Where-Object Model -match 'ssd'
-        If ($IsSSD -eq $Null) { #SSD Check
-            Optimize-Volume -DriveLetter C -Defrag
-        }
+    # Step 5 - Defrag
+    Write-Verbose "Step 5 - Defrag"
+    $IsSSD = Get-Disk | Where-Object Model -match 'ssd'
+    If ($IsSSD -eq $Null) { #SSD Check
+        Optimize-Volume -DriveLetter C -Defrag
+    }
 
-        # Step 6 - Drive Health Check
-        If ((Get-Disk).HealthStatus -ne 'Healthy') { #Failing Drive Check
-            Write-Output "A hard drive in this computer might be failing, it is suggested you investigate all connected hard drives."
-        }
-    } Else {
-        Write-Output "This script is designed only to be run on Windows 10"
+    # Step 6 - Drive Health Check
+    Write-Verbose "Step 6 - Drive Health Check"
+    If ((Get-Disk).HealthStatus -ne 'Healthy') { #Failing Drive Check
+        Write-Output "A hard drive in this computer might be failing, it is suggested you investigate all connected hard drives."
     }
 } Else {
-    Write-Output "This script requires to be run in an elevated shell"
+    Write-Error "Prerequisite Check Failed - This script can only be run on Windows 10 in an elevated shell"
 }
 
 Write-Output "End of TronNG - $(Get-Date)"
